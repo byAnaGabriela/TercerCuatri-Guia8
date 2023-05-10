bb75a85a13ed8070b3237ee23f0b42f1203e2733
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:53:43 -0300

    Clase Main ej4
    Enunciado

---
+++ TercerCuatri-Guia8/src/cuatroTest/Main.java
@@ -0,0 +1,10 @@
+package cuatroTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase genérica llamada Caja que pueda almacenar cualquier tipo de objeto y tenga un método
+        para obtener el objeto almacenado en la caja.*/
+
+    }
+}
