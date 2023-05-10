6dc3bdf6419088fb0e31a9edace2d7ee1c9166cc
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:52:52 -0300

    Clase Main ej1
    Enunciado

---
+++ TercerCuatri-Guia8/src/unoTest/Main.java
@@ -0,0 +1,11 @@
+package unoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase genérica llamada MiLista que pueda almacenar cualquier tipo de objeto y tenga un método
+        para agregar elementos a la lista y otro método para obtener el elemento en una posición específica de
+        la lista.*/
+
+    }
+}
