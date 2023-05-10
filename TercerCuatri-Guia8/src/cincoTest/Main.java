b5bb8636c06321d49e10bbd42b755a99ce31d0be
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:53:58 -0300

    Clase Main ej5
    Enunciado

---
+++ TercerCuatri-Guia8/src/cincoTest/Main.java
@@ -0,0 +1,10 @@
+package cincoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase genérica llamada Pila que pueda almacenar cualquier tipo de objeto y tenga un método para
+        agregar elementos a la pila y otro método para sacar el elemento en la parte superior de la pila.*/
+
+    }
+}
