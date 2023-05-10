5eba1ce4a9b55dc0c05fb88398bf649fc6afaf98
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:53:04 -0300

    Clase Main ej3
    Enunciado

---
+++ TercerCuatri-Guia8/src/tresTest/Main.java
@@ -0,0 +1,11 @@
+package tresTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase genérica llamada Mapa que pueda almacenar cualquier tipo de objeto como clave y
+        cualquier tipo de objeto como valor, y tenga métodos para agregar un par clave-valor al mapa y
+        obtener el valor asociado con una clave específica.*/
+
+    }
+}
