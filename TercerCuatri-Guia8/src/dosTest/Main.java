a2e71ad518330f538da1183f9aa149efc3b55d4c
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:53:29 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia8/src/dosTest/Main.java
@@ -0,0 +1,10 @@
+package dosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una interfaz genérica llamada Comparador que tenga un método para comparar dos objetos de cualquier
+        tipo y devolver el resultado de la comparación.*/
+
+    }
+}
